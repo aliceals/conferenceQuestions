e1a70a7a56d84057d527072a4ab27d279d61aa25
Alice Alsford <alice.alsford@gmail.com>
2020-01-23 21:12:18 +1300

    set up db for deploy

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -27,11 +27,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
