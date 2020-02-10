3f2f43b6a547a73e6296aebb0715352b6798c60a
Alice Alsford <alice.alsford@gmail.com>
2020-02-10 15:24:10 +1300

    styled button

--- client/components/DisplayQuestion.jsx
+++ client/components/DisplayQuestion.jsx
@@ -49,10 +49,10 @@ class DisplayQuestion extends React.Component {
                 <h2>Current questions</h2>
                 <ul>
                     {this.props.questions.map((questions, i) => {
-                        return <li key={i}>{questions.question_string.charAt(0).toUpperCase() + questions.question_string.substring(1)} <button name={questions.question_id}
+                        return <li key={i}>{questions.question_string.charAt(0).toUpperCase() + questions.question_string.substring(1)} <button name={questions.question_id} className="delete"
                             onClick={this.deleteQuestion}>delete</button>
-                            {questions.starred ? <button className="star" name={questions.question_id} value={0} onClick={this.star}>⭐</button>
-                                : <button className="star" name={questions.question_id} value={1} onClick={this.star}>☆</button>}<button name={questions.question_id} value={questions.votes} onClick={this.upVote}>⬆</button>
+                            {questions.starred ? <button className="button" name={questions.question_id} value={0} onClick={this.star}>⭐</button>
+                                : <button className="button" name={questions.question_id} value={1} onClick={this.star}>☆</button>}<button name={questions.question_id} value={questions.votes} className="button" onClick={this.upVote}>⬆</button>
                             Votes: {questions.votes}</li>
                     })}
                 </ul>
